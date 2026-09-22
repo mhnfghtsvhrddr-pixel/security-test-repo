9faafb1e176ec92c12362498e2bdb19b516a9b3c
mhnfghtsvhrddr-pixel <mhnfghtsvhrddr@gmail.com>
2026-09-23 01:47:15 +0300

    security: Replace insecure MD5 password hashing with bcrypt

--- vulnerable_code.py
+++ vulnerable_code.py
@@ -1,8 +1,7 @@
 import hashlib
 
 
-def hash_password(password):
-    return hashlib.md5(password.encode()).hexdigest()
+import bcrypt\n\ndef hash_password(password: str) -> str:\n    # bcrypt automatically salts and applies a work factor (default 12)\n    hashed = bcrypt.hashpw(password.encode(), bcrypt.gensalt())\n    return hashed.decode()
 
 
 def get_user(username, cursor):
