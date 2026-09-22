13c8bb131f07c4846e3e05171ac6094c0361f296
mhnfghtsvhrddr-pixel <mhnfghtsvhrddr@gmail.com>
2026-09-23 02:32:27 +0300

    security: Replace insecure MD5 hashing with a secure password hashing algorithm

--- vulnerable_code.py
+++ vulnerable_code.py
@@ -2,7 +2,11 @@ import hashlib
 
 
 def hash_password(password):
-    return hashlib.md5(password.encode()).hexdigest()
+    # Use PBKDF2 with SHA‑256, 100,000 iterations and a random salt
+    import os, hashlib, binascii
+    salt = os.urandom(16)
+    dk = hashlib.pbkdf2_hmac('sha256', password.encode(), salt, 100000)
+    return binascii.hexlify(salt + dk).decode()
 
 
 def get_user(username, cursor):
