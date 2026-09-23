6c8afe114b68b3e2f06ed7f4560340591b7c34da
mhnfghtsvhrddr-pixel <mhnfghtsvhrddr@gmail.com>
2026-09-23 18:23:48 +0300

    security: Fix SQL injection in get_user by using parameterized query

--- vulnerable_code.py
+++ vulnerable_code.py
@@ -1,11 +1,10 @@
 import hashlib
 
 
-def hash_password(password):
-    return hashlib.md5(password.encode()).hexdigest()
+import argon2\n\nph = argon2.PasswordHasher()\n\ndef hash_password(password: str) -> str:\n    \"\"\"Hash a password using Argon2id.\n    Argon2 provides resistance against GPU/ASIC cracking and includes a random salt.\n    \"\"\"\n    return ph.hash(password)\n
 
 
 def get_user(username, cursor):
-    query = "SELECT * FROM users WHERE name = '" + username + "'"
-    cursor.execute(query)
+    query = "SELECT * FROM users WHERE name = %s"
+    cursor.execute(query, (username,))
     return cursor.fetchone()
